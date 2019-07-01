6985dd2938b4c46672151f5d7737c8ede21b8363
SuitcaseCoder <lauraruizroehrs@gmail.com>
2019-06-30 23:38:00 -0500

    testing credentials

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/Words';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/words';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test';
 
 exports.PORT = process.env.PORT || 8080;
