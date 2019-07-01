e029ca68f60aef963e2c34677b8f2bbffa3ec8a9
SuitcaseCoder <lauraruizroehrs@gmail.com>
2019-06-30 23:34:46 -0500

    fixed config files

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://lexiUser:lexi123@lexi-rrqwl.mongodb.net/test?retryWrites=true&w=majority'
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb+srv://lexiUser:lexi123@lexi-rrqwl.mongodb.net/Words?retryWrites=true&w=majority';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/Words';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test';
+
 exports.PORT = process.env.PORT || 8080;
+
